26f1e5240d74479cbdff21fd22268eaafe74f259
Luca mancuso <105869480+lukpeluk@users.noreply.github.com>
2026-03-04 01:03:25 -0300

    Add useful urls like the homepage and updating urls

--- immich-storage-to-profile.user.js
+++ immich-storage-to-profile.user.js
@@ -3,10 +3,14 @@
 // @description  Moves the immich storage indicator to the profile menu, like it is in the mobile version.
 // @match        https://<your-immich-instance>/*
 // @match        https://demo.immich.app/*
-// @version      1.0
-// @author       Luk
+// @version      1.0.1
+// @author       Lukpeluk
 // @namespace    lukpeluk
 // @icon         https://immich.app/favicon.ico
+// @homepageURL  https://github.com/lukpeluk/immich-storage-to-profile/issues
+// @supportURL   https://github.com/lukpeluk/immich-storage-to-profile/issues
+// @updateURL    https://raw.githubusercontent.com/lukpeluk/immich-storage-to-profile/main/immich-storage-to-profile.user.js
+// @downloadURL  https://raw.githubusercontent.com/lukpeluk/immich-storage-to-profile/main/immich-storage-to-profile.user.js
 // @grant        none
 // ==/UserScript==
 
